857ecab44f03a078e2e575eac25dcbaeafad0cf1
Edith Chow <Edith.Chow@arup.com>
2021-01-08 11:15:19 -0500

    Added try/catch exception, added saving of CSV per frame just to prevent data loss for any reason. Started adding in data aggregation.

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ if not os.path.exists(output_folder_path):
 persons_dir = [f.name for f in os.scandir(extraction_folder_path) if f.is_dir()]
 
 # Set up arrays for collecting information:
-person_id = []
+person_ids = []
 frame_number = []
 person_label = []
 person_label_probability = []
@@ -227,141 +227,190 @@ for person_id in persons_dir:
 
         print('Processing ID{0} at frame {1}'.format(person_id, curr_frame_num))
 
+        # Get output image path, skip drawing and saving if it already exists:
+        img_full_path = os.path.join(output_folder_path,person_id)
+        if not os.path.exists(img_full_path):
+            os.mkdir(img_full_path)
+        img_full_path = os.path.join(img_full_path, '{0}_result.png'.format(curr_frame_num))
+        print(img_full_path)
+        is_img_output_exists = os.path.exists(img_full_path)
+
         img = cv2.imread(img_path)
-        # Pad to 1920x1080
-        img = add_padding(img, 1920, 1080, (0,0,0))
+        try:
+            if type(img) == None:
+                continue
+            # Pad to 1920x1080
+            img = add_padding(img, 1920, 1080, (0,0,0))
 
-        X, ratio = format_img(img, C)
+            X, ratio = format_img(img, C)
 
-        if K.image_data_format() == 'channels_last':
-            X = np.transpose(X, (0, 2, 3, 1))
+            if K.image_data_format() == 'channels_last':
+                X = np.transpose(X, (0, 2, 3, 1))
 
-        # get the feature maps and output from the RPN
-        [Y1, Y2, F] = model_rpn.predict(X)
-        
+            # get the feature maps and output from the RPN
+            [Y1, Y2, F] = model_rpn.predict(X)
+            
 
-        R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_data_format(), overlap_thresh=0.7)
+            R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_data_format(), overlap_thresh=0.7)
 
-        # convert from (x1,y1,x2,y2) to (x,y,w,h)
-        R[:, 2] -= R[:, 0]
-        R[:, 3] -= R[:, 1]
+            # convert from (x1,y1,x2,y2) to (x,y,w,h)
+            R[:, 2] -= R[:, 0]
+            R[:, 3] -= R[:, 1]
 
-        # apply the spatial pyramid pooling to the proposed regions
-        bboxes = {}
-        probs = {}
+            # apply the spatial pyramid pooling to the proposed regions
+            bboxes = {}
+            probs = {}
 
-        for jk in range(R.shape[0]//C.num_rois + 1):
-            ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
-            if ROIs.shape[1] == 0:
-                break
+            for jk in range(R.shape[0]//C.num_rois + 1):
+                ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
+                if ROIs.shape[1] == 0:
+                    break
 
-            if jk == R.shape[0]//C.num_rois:
-                #pad R
-                curr_shape = ROIs.shape
-                target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
-                ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
-                ROIs_padded[:, :curr_shape[1], :] = ROIs
-                ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
-                ROIs = ROIs_padded
+                if jk == R.shape[0]//C.num_rois:
+                    #pad R
+                    curr_shape = ROIs.shape
+                    target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
+                    ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+                    ROIs_padded[:, :curr_shape[1], :] = ROIs
+                    ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+                    ROIs = ROIs_padded
 
-            [P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+                [P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
 
-            for ii in range(P_cls.shape[1]):
+                for ii in range(P_cls.shape[1]):
 
-                if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
-                    continue
+                    if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+                        continue
 
-                cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+                    cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
 
-                if cls_name not in bboxes:
-                    bboxes[cls_name] = []
-                    probs[cls_name] = []
+                    if cls_name not in bboxes:
+                        bboxes[cls_name] = []
+                        probs[cls_name] = []
 
-                (x, y, w, h) = ROIs[0, ii, :]
+                    (x, y, w, h) = ROIs[0, ii, :]
 
-                cls_num = np.argmax(P_cls[0, ii, :])
-                try:
-                    (tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
-                    tx /= C.classifier_regr_std[0]
-                    ty /= C.classifier_regr_std[1]
-                    tw /= C.classifier_regr_std[2]
-                    th /= C.classifier_regr_std[3]
-                    x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
-                except:
-                    pass
-                bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
-                probs[cls_name].append(np.max(P_cls[0, ii, :]))
+                    cls_num = np.argmax(P_cls[0, ii, :])
+                    try:
+                        (tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+                        tx /= C.classifier_regr_std[0]
+                        ty /= C.classifier_regr_std[1]
+                        tw /= C.classifier_regr_std[2]
+                        th /= C.classifier_regr_std[3]
+                        x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
+                    except:
+                        pass
+                    bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
+                    probs[cls_name].append(np.max(P_cls[0, ii, :]))
 
-        all_dets = []
+            all_dets = []
 
-        # Ideally there should only be one box because we're aiming to pass in an image of a single person, but the bbox in the crop
-        # may include people in the back/foreground so we'll have to make a decision.
-        #
-        # We'll try this:
-        #   Combination of confidence (probability) and larger bbox area
-        key_data = pd.DataFrame()
-        keys = []
-        nKeys = []
-        minProbKeys = []
-        maxProbKeys = []
-        avgProbKeys = []
-        for key in bboxes:
-            bbox = np.array(bboxes[key])
+            # Ideally there should only be one box because we're aiming to pass in an image of a single person, but the bbox in the crop
+            # may include people in the back/foreground so we'll have to make a decision.
+            #
+            # We'll try this:
+            #   Combination of confidence (probability) and larger bbox area
+            key_data = pd.DataFrame()
+            keys = []
+            nKeys = []
+            minProbKeys = []
+            maxProbKeys = []
+            avgProbKeys = []
+            for key in bboxes:
+                bbox = np.array(bboxes[key])
 
-            # Get the bounding boxes and associated probabilites of all detection that falls under this 'key':
-            new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
+                # Get the bounding boxes and associated probabilites of all detection that falls under this 'key':
+                new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
 
-            # Store this for decision-making:
-            keys.append(key)
-            nKeys.append(len(new_boxes))
-            minProbKeys.append(min(new_probs))
-            maxProbKeys.append(max(new_probs))
-            avgProbKeys.append(sum(new_probs)/len(new_probs))
+                # Store this for decision-making:
+                keys.append(key)
+                nKeys.append(len(new_boxes))
+                minProbKeys.append(min(new_probs))
+                maxProbKeys.append(max(new_probs))
+                avgProbKeys.append(sum(new_probs)/len(new_probs))
 
-            # Draw the detection onto the images:
-            for jk in range(new_boxes.shape[0]):
-                (x1, y1, x2, y2) = new_boxes[jk,:]
+                # Draw the detection onto the images:
+                for jk in range(new_boxes.shape[0]):
+                    (x1, y1, x2, y2) = new_boxes[jk,:]
 
-                (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+                    (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
 
-                cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
+                    # Don't bother with drawing if this has already been done:
+                    if not is_img_output_exists:
+                        cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
 
-                textLabel = f'{key}: {int(100*new_probs[jk])}'
-                all_dets.append((key,100*new_probs[jk]))
+                    textLabel = f'{key}: {int(100*new_probs[jk])}'
+                    all_dets.append((key,100*new_probs[jk]))
 
-                (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
-                textOrg = (real_x1, real_y1-0)
+                    # Don't bother with drawing if this has already been done:
+                    if not is_img_output_exists:
+                        (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+                        textOrg = (real_x1, real_y1-0)
 
-                cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
-                cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
-                cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+                        cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
+                        cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
+                        cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
 
-        key_data = pd.DataFrame(data={'key': keys, 'count': nKeys, 'min_prob': minProbKeys, 'max_prob': maxProbKeys, 'avg_prob': avgProbKeys})
-        if not key_data.empty:
-            # No classification, log
-            prevalent_key = key_data[key_data['count'] == max(key_data['count'])]
-            if prevalent_key.empty:
-                person_label.append('NA')
-                person_label_probability.append(-1)
-            elif len(prevalent_key) == 1:
-                person_label.append(prevalent_key['label'])
-                person_label_probability.append(prevalent_key['avg_prob'])
-            else:
-                # Take the one with the higher average probability:
-                prevalent_key = prevalent_key[prevalent_key['avg_prob'] == max(prevalent_key['avg_prob'])]
-                person_label.append(prevalent_key['label'])
-                person_label_probability.append(prevalent_key['avg_prob'])
+            key_data = pd.DataFrame(data={'key': keys, 'count': nKeys, 'min_prob': minProbKeys, 'max_prob': maxProbKeys, 'avg_prob': avgProbKeys})
+            if not key_data.empty:
+                # No classification, log
+                prevalent_key = key_data[key_data['count'] == max(key_data['count'])]
+                if prevalent_key.empty:
+                    person_label.append('NA')
+                    person_label_probability.append(-1)
+                elif len(prevalent_key) == 1:
+                    person_label.append(prevalent_key['key'])
+                    person_label_probability.append(prevalent_key['avg_prob'])
+                else:
+                    # Take the one with the higher average probability:
+                    prevalent_key = prevalent_key[prevalent_key['avg_prob'] == max(prevalent_key['avg_prob'])]
+                    person_label.append(prevalent_key['key'])
+                    person_label_probability.append(prevalent_key['avg_prob'])
+                
+                person_ids.append(person_id)
+                frame_number.append(curr_frame_num)
 
-        print(f'Elapsed time = {time.time() - st}')
-        
-        print(all_dets)
-        
-        # Write image to the output folder in the subdirectory with ID, append '_result' at the end:
-        cv2.imwrite('{0}/{1}_result.png'.format(os.path.join(output_folder_path,person_id),curr_frame_num),img)
+            print(f'Elapsed time = {time.time() - st}')
+            
+            print(all_dets)
+            
+            # Write image to the output folder in the subdirectory with ID, append '_result' at the end:
+            if not is_img_output_exists:
+                cv2.imwrite(img_full_path,img)
+
+            # Just save/update the csv:
+            dfc = pd.DataFrame(data={'id': person_ids, 'frame': frame_number, 'label': person_label, 'probability': person_label_probability})
+            dfc.to_csv(os.path.join(output_folder_path, 'labelled_data.csv'))
+        except:
+            traceback.print_exc()
+            continue
 
 
 # Dump data into dataframe and save:
     
 # Set up arrays for collecting information:
-dfc = pd.DataFrame(data={'id': person_id, 'frame': frame_number, 'label': person_label, 'probability': person_label_probability})
+dfc = pd.DataFrame(data={'id': person_ids, 'frame': frame_number, 'label': person_label, 'probability': person_label_probability})
 dfc.to_csv(os.path.join(output_folder_path, 'labelled_data.csv'))
+
+
+# Aggregate per person and get the final labels:
+#   ID | Label (%) | Total Images | Total Labelled
+person_ids = []
+person_labels = []
+total_images = []
+total_images_labelled = []
+for id in persons_dir:
+    dfp = dfc[dfc['id'] == id]
+    # Get total images in folder:
+    person_path = os.path.join(extraction_folder_path, person_id)
+
+    # Get all the files:
+    nTotal = len(os.listdir(person_path))
+    total_images.append(nTotal)
+
+    if dfp.empty:
+        # No labels
+        person_ids.append(id)
+        person_labels.append('NA')
+        total_images_labelled.append(0)
+    else:
